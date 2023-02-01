d7833801c9b07d23a2611876a09756460ee67b7c
Al0cam <benjaminsikac@gmail.com>
2023-02-01 22:36:53 +0100

    defined tone and emoji

---
+++ src/components/emojiItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const emoji = "&#x1F64F;";
+
+const lightTone = "&#x1F3FB;";
+const mediumLightTone = "&#x1F3FC;";
+const mediumTone = "&#x1F3FD;";
+const mediumDarkTone = "&#x1F3FE;";
+const darkTone = "&#x1F3FF;";
+</script>
+
+<template>
+  <h1 v-html="emoji"></h1>
+</template>
