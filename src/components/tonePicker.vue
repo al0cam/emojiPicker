3c38f32cfa2ca7a754e42cb622553d9630e2148c
Al0cam <benjaminsikac@gmail.com>
2023-02-04 15:57:40 +0100

    adde new component tone picker

---
+++ src/components/tonePicker.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps<{
+  emoji: {
+    type: string;
+    required: true;
+  };
+}>();
+</script>
+
+<template></template>
