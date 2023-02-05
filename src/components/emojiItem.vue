01c798a47cb9a233fdd7f6b8af9af155efa055a6
Al0cam <benjaminsikac@gmail.com>
2023-02-05 10:10:09 +0100

    added reactivity to the tone picker

--- src/components/emojiItem.vue
+++ src/components/emojiItem.vue
@@ -1,26 +1,75 @@
 <script setup lang="ts">
-const pray = "&#x1F64F;";
+import { defineProps, ref } from "vue";
 
-const lightTone = "&#x1F3FB;";
-const mediumLightTone = "&#x1F3FC;";
-const mediumTone = "&#x1F3FD;";
-const mediumDarkTone = "&#x1F3FE;";
-const darkTone = "&#x1F3FF;";
+const props = defineProps<{
+  emoji: {
+    name: string;
+    value: string;
+  };
+}>();
+
+const tones = [
+  {
+    name: "Vanilla Tone",
+    value: "",
+  },
+  {
+    name: "Light Tone",
+    value: "&#x1F3FB;",
+  },
+  {
+    name: "Medium Light Tone",
+    value: "&#x1F3FC;",
+  },
+  {
+    name: "Medium Tone",
+    value: "&#x1F3FD;",
+  },
+  {
+    name: "Medium Dark Tone",
+    value: "&#x1F3FE;",
+  },
+  {
+    name: "Dark Tone",
+    value: "&#x1F3FF;",
+  },
+];
 
 function pickTone(emoji: string) {
   return function (tone: string) {
     return emoji + tone;
   };
 }
+const pickToneForEmoji = pickTone(props.emoji.value);
+
+const toneContainerHidden = ref(true);
 </script>
 
 <template>
+  <div id="toneContainer" v-if="!toneContainerHidden">
+    <span
+      v-for="tone in tones"
+      :key="tone.name"
+      :title="emoji.name + ' ' + tone.name"
+      v-html="pickToneForEmoji(tone.value)"
+    >
+    </span>
+  </div>
   <h1>
-    <span v-html="pray"></span>
-    <span v-html="pickTone(pray)(lightTone)"></span>
-    <span v-html="pickTone(pray)(mediumLightTone)"></span>
-    <span v-html="pickTone(pray)(mediumTone)"></span>
-    <span v-html="pickTone(pray)(mediumDarkTone)"></span>
-    <span v-html="pickTone(pray)(darkTone)"></span>
+    <span
+      v-html="emoji.value"
+      :title="emoji.name"
+      @click="toneContainerHidden = !toneContainerHidden"
+    ></span>
   </h1>
 </template>
+
+<style scoped>
+toneContainer {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  place-items: center;
+  place-content: center;
+}
+</style>
